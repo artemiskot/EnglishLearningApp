0cd436591456deb1cb5b205bba286ff20a3f3299
artemiskot <artem@kotelevsky.ru>
2022-05-02 14:10:53 +0300

    Fragments + ROOM added

--- app/src/main/java/com/englishlearningapp/MainActivity.kt
+++ app/src/main/java/com/englishlearningapp/MainActivity.kt
@@ -2,10 +2,40 @@ package com.englishlearningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.englishlearningapp.databinding.ActivityBottomNavBinding
+import com.englishlearningapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private var solved: Int = 0
+    private var mistakes: Int = 0
+    private lateinit var binding: ActivityBottomNavBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityBottomNavBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_bottom_nav)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_learn, R.id.navigation_practice, R.id.navigation_stats
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
